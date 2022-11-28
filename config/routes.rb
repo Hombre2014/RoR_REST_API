b43f069b0c267cc939987f42405d3dcffc25ba37
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-11-28 16:50:43 +0100

    Add routing and requests tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  # get 'articles', to: 'articles#index'
+  resources :articles, only: [:index, :show]
   # root "articles#index"
 end
